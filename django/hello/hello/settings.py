456014d497f8df6d60eca4c2dea178f10be455fd
Alex <matvich1999@gmail.com>
2021-09-08 16:42:56 +0300

    modified setting.py

--- django/hello/hello/settings.py
+++ django/hello/hello/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp',
-    'django_tables2'
+    'django_tables2',
+    'django_matplotlib'
 ]
 
 MIDDLEWARE = [
